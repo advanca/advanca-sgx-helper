2701fc576ea3ebdefb51b434ada4764442d07355
Chua Zheng Leong <czl@iiyume.org>
2020-08-03 06:30:11 +0000

    Add sr25519 crypto operation + test

--- advanca-crypto/src/advanca_cryptolib/mod.rs
+++ advanca-crypto/src/advanca_cryptolib/mod.rs
@@ -15,7 +15,13 @@ use sgx_types::*;
 
 use std::vec::Vec;
 
+use rand::rngs::StdRng;
+use rand::SeedableRng;
+use schnorrkel;
+
 use advanca_macros::handle_sgx;
+
+const SIGNING_CONTEXT: &[u8] = b"advanca-sign";
 
 pub fn aes128cmac_mac(p_key: &Aes128Key, p_data: &[u8]) -> Result<Aes128Mac, CryptoError> {
     match rsgx_rijndael128_cmac_slice(&p_key.key, p_data) {
@@ -108,7 +114,7 @@ pub fn secp256r1_gen_keypair() -> Result<(Secp256r1PrivateKey, Secp256r1PublicKe
     Ok((prvkey, pubkey))
 }
 
-fn secp256r1_compute_shared_dhkey(
+pub fn secp256r1_compute_shared_dhkey(
     prvkey: &Secp256r1PrivateKey,
     pubkey: &Secp256r1PublicKey,
 ) -> Result<[u8; 32], CryptoError> {
@@ -129,6 +135,55 @@ fn secp256r1_compute_shared_dhkey(
     }
     Ok(gab_x.s)
 }
+
+pub fn sr25519_gen_keypair() -> Result<(Sr25519PrivateKey, Sr25519PublicKey), CryptoError> {
+    let mut seed_bytes = [0_u8; 32];
+    unsafe {
+        handle_sgx!(sgx_read_rand(seed_bytes.as_mut_ptr(), seed_bytes.len()))?;
+    }
+    let rng: StdRng = SeedableRng::from_seed(seed_bytes);
+    let sr25519_keypair = schnorrkel::Keypair::generate_with(rng);
+    let sr25519_pubkey = Sr25519PublicKey::from_schnorrkel_public(&sr25519_keypair.public);
+    let sr25519_prvkey = Sr25519PrivateKey::from_schnorrkel_private(&sr25519_keypair.secret);
+    Ok((sr25519_prvkey, sr25519_pubkey))
+}
+
+pub fn sr25519_sign_msg(
+    prvkey: &Sr25519PrivateKey,
+    msg: &[u8],
+) -> Result<Sr25519SignedMsg, CryptoError> {
+    let mut seed_bytes = [0_u8; 32];
+    unsafe {
+        handle_sgx!(sgx_read_rand(seed_bytes.as_mut_ptr(), seed_bytes.len()))?;
+    }
+    let rng: StdRng = SeedableRng::from_seed(seed_bytes);
+    let secretkey = prvkey.to_schnorrkel_private();
+    let context = schnorrkel::signing_context(SIGNING_CONTEXT);
+    let signature = secretkey.sign(schnorrkel::context::attach_rng(context.bytes(msg), rng), &secretkey.to_public());
+    Ok(Sr25519SignedMsg {
+        msg: msg.to_vec(),
+        signature: Sr25519Signature::from_schnorrkel_signature(&signature),
+    })
+}
+
+pub fn sr25519_verify_msg(
+    pubkey: &Sr25519PublicKey,
+    signed_msg: &Sr25519SignedMsg,
+) -> Result<bool, CryptoError> {
+    sr25519_verify_signature(pubkey, &signed_msg.msg, &signed_msg.signature)
+}
+
+pub fn sr25519_verify_signature(
+    pubkey: &Sr25519PublicKey,
+    msg: &[u8],
+    signature: &Sr25519Signature,
+) -> Result<bool, CryptoError> {
+    let context = schnorrkel::signing_context(SIGNING_CONTEXT);
+    let schnorrkel_pubkey = pubkey.to_schnorrkel_public();
+    let schnorrkel_signature = signature.to_schnorrkel_signature();
+    Ok(schnorrkel_pubkey.verify(context.bytes(msg), &schnorrkel_signature).is_ok())
+}
+
 
 pub fn derive_kdk(
     prvkey: &Secp256r1PrivateKey,
@@ -318,5 +373,18 @@ mod tests {
             ],
         };
         assert_eq!(prvkey, prvkey_actual);
+    }
+
+    #[test]
+    fn sr25519_test() {
+        let (sr25519_prvkey, sr25519_pubkey) = sr25519_gen_keypair().unwrap();
+        let msg: &[u8] = b"test message";
+        let mut signed_msg = sr25519_sign_msg(&sr25519_prvkey, msg).unwrap();
+        assert_eq!(signed_msg.msg, msg);
+        let is_verified = sr25519_verify_msg(&sr25519_pubkey, &signed_msg).unwrap();
+        assert_eq!(true, is_verified);
+        signed_msg.msg[0] = 0;
+        let is_verified = sr25519_verify_msg(&sr25519_pubkey, &signed_msg).unwrap();
+        assert_eq!(false, is_verified);
     }
 }
