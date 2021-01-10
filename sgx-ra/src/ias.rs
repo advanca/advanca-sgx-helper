4d5c9750c970c5c5cf7565b74937e460212683f4
Chua Zheng Leong <czl@iiyume.org>
2021-01-10 21:50:18 +0800

    Add parsing for platform_info_blob

--- sgx-ra/src/ias.rs
+++ sgx-ra/src/ias.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::mem::transmute;
+
 use base16;
 use base64;
 use reqwest;
@@ -27,6 +29,10 @@ use strum_macros::{Display, EnumString};
 use std::error::Error;
 
 use crate::sgx_ra::SgxQuote;
+use advanca_crypto_types::*;
+
+use bitflags::bitflags;
+use sgx_types::*;
 
 pub struct IasServer {
     api_key: String,
@@ -85,6 +91,125 @@ impl IasServer {
             .unwrap();
         let sigrl_base64 = res.text().unwrap();
         base64::decode(sigrl_base64).unwrap()
+    }
+}
+
+bitflags! {
+    /* Masks for sgx_epid_group_flags*/
+    #[derive(Default, Serialize, Deserialize)]
+    pub struct SgxEpidGroupFlags: u8 {
+        const QE_EPID_GROUP_REVOKED                  = 0b00000001;
+        const PERF_REKEY_FOR_QE_EPID_GROUP_AVAILABLE = 0b00000010;
+        const QE_EPID_GROUP_OUT_OF_DATE              = 0b00000100;
+    }
+}
+
+bitflags! {
+    /* Masks for sgx_tcb_evaluation_flags*/
+    #[derive(Default, Serialize, Deserialize)]
+    pub struct SgxTcbEvaluationFlags: u16 {
+        const QUOTE_CPUSVN_OUT_OF_DATE      = 0b00000001;
+        const QUOTE_ISVSVN_QE_OUT_OF_DATE   = 0b00000010;
+        const QUOTE_ISVSVN_PCE_OUT_OF_DATE  = 0b00000100;
+        /* the EPID signature of the ISV enclave QUOTE has been verified correctly but additional
+         * configuration of SGX platform may be needed.*/
+        const PLATFORM_CONFIGURATION_NEEDED = 0b00001000;
+    }
+}
+
+bitflags! {
+    /* Masks for sgx_pse_evaluation_flags*/
+    #[derive(Default, Serialize, Deserialize)]
+    pub struct PseEvaluationFlags: u16 {
+        /* PS_SEC_PROP_DESC.PSE_ISVSVN is out of date*/
+        const PSE_ISVSVN_OUT_OF_DATE                          = 0b00000001;
+        /* CSME EPID 1.1 group identified by PS_SEC_PROP_DESC. PS_HW_GID has been revoked*/
+        const EPID_GROUP_ID_BY_PS_HW_GID_REVOKED              = 0b00000010;
+        /* PSDA SVN indicated in PS_SEC_PROP_DESC.PS_HW_SEC_INFO is out of date*/
+        const SVN_FROM_PS_HW_SEC_INFO_OUT_OF_DATE             = 0b00000100;
+        /* CSME EPID 1.1 SigRL version indicated in PS_SEC_PROP_DESC. PS_HW_SIG_RLver is out of
+         * date*/
+        const SIGRL_VER_FROM_PS_HW_SIG_RLVER_OUT_OF_DATE      = 0b00001000;
+        /* CSME EPID 1.1 PrivRL version indicated in PS_SEC_PROP_DESC. PS_HW_PrivKey_RLver is out
+         * of date*/
+        const PRIVRL_VER_FROM_PS_HW_PRV_KEY_RLVER_OUT_OF_DATE = 0b00010000;
+    }
+}
+
+const SGX_CPUSVN_SIZE       : usize = 16;
+const PSVN_SIZE             : usize = 18; // sizeof(psvn_t)
+const PSDA_SVN_SIZE         : usize = 4;
+const ISVSVN_SIZE           : usize = 2;
+const SGX_PLATFORM_INFO_SIZE: usize = 101;
+
+#[allow(non_camel_case_types)]
+type sgx_isv_svn_t = u16; // 2 bytes
+#[allow(non_camel_case_types)]
+type tcb_psvn_t = [u8; PSVN_SIZE];
+#[allow(non_camel_case_types)]
+type psda_svn_t = [u8; PSDA_SVN_SIZE];
+#[allow(non_camel_case_types)]
+type pse_isvsvn_t = [u8; ISVSVN_SIZE];
+
+#[derive(Copy, Clone, Debug)]
+#[repr(packed)]
+#[allow(non_camel_case_types)]
+struct sgx_cpu_svn_t {
+    // 16 bytes
+    svn: [u8; SGX_CPUSVN_SIZE],
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(packed)]
+#[allow(non_camel_case_types)]
+struct psvn_t {
+    // 16 + 2
+    cpu_svn: sgx_cpu_svn_t,
+    isv_svn: sgx_isv_svn_t,
+}
+
+#[derive(Copy, Clone)]
+#[repr(packed)]
+#[allow(non_camel_case_types)]
+struct platform_info_blob {
+    sgx_epid_group_flags: u8,
+    sgx_tcb_evaluation_flags: u16,
+    pse_evaluation_flags: u16,
+    latest_equivalent_tcb_psvn: tcb_psvn_t,
+    latest_pse_isvsvn: pse_isvsvn_t,
+    latest_psda_svn: psda_svn_t,
+    xeid: u32,
+    gid: u32,
+    signature: sgx_ec256_signature_t,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct IasPlatformInfoBlob {
+    sgx_epid_group_flags: SgxEpidGroupFlags,
+    sgx_tcb_evaluation_flags: SgxTcbEvaluationFlags,
+    pse_evaluation_flags: PseEvaluationFlags,
+    latest_equivalent_tcb_psvn: [u8; PSVN_SIZE],
+    latest_pse_isvsvn: [u8; ISVSVN_SIZE],
+    latest_psda_svn: [u8; PSDA_SVN_SIZE],
+    xeid: u32,
+    gid: u32,
+    signature: Secp256r1Signature,
+}
+
+impl From<[u8; SGX_PLATFORM_INFO_SIZE]> for IasPlatformInfoBlob {
+    fn from(blob_bytes: [u8; SGX_PLATFORM_INFO_SIZE]) -> Self {
+        let parsed_blob: platform_info_blob = unsafe{transmute(blob_bytes)};
+        IasPlatformInfoBlob {
+            sgx_epid_group_flags: SgxEpidGroupFlags::from_bits(parsed_blob.sgx_epid_group_flags).unwrap(),
+            sgx_tcb_evaluation_flags: SgxTcbEvaluationFlags::from_bits(parsed_blob.sgx_tcb_evaluation_flags).unwrap(),
+            pse_evaluation_flags: PseEvaluationFlags::from_bits(parsed_blob.pse_evaluation_flags).unwrap(),
+            latest_equivalent_tcb_psvn: parsed_blob.latest_equivalent_tcb_psvn,
+            latest_pse_isvsvn: parsed_blob.latest_pse_isvsvn,
+            latest_psda_svn: parsed_blob.latest_psda_svn,
+            xeid: parsed_blob.xeid,
+            gid: parsed_blob.gid,
+            signature: parsed_blob.signature.into()
+        }
     }
 }
 
